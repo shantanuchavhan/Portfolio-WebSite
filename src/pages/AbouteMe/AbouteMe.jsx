e80e4b4adfca59c5a0fe6b6dad384f936268040b
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-24 21:40:26 +0530

    commit

--- src/pages/AbouteMe/AbouteMe.jsx
+++ src/pages/AbouteMe/AbouteMe.jsx
@@ -4,7 +4,14 @@ import bg from "../../images/pexels-pixabay-326333.jpg"
 import { motion } from 'framer-motion'
 const AbouteMe = () => {
   return (
-    <div className='relative flex flex-col h-screen p-8  text-white '>
+    <div className='relative flex flex-col h-screen  text-white '>
+      <motion.div
+        initial={{ opacity: 0, x: 0}}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 3, ease: "easeInOut" }}  
+      
+      >
+      <div className='p-8'>
       <div className='flex justify-between items-end pl-3 h-1/6 border-b border-dotted border-[#f8fee9] p-4'>
         <h1 className='text-4xl m-2'>About me</h1>
         <div>
@@ -24,6 +31,8 @@ const AbouteMe = () => {
             </div>
         </div>
       </div>
+      </div>
+      </motion.div>
       <div className='absolute top-0 left-0 z-[-1] w-screen '>
         <img  src={bg} alt="" />
       </div>
