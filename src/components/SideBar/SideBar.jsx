1ffe5a70c3f99c54f467f8bfe3b075c027cdab00
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-26 21:44:35 +0530

    commit

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -54,10 +54,10 @@ const SideBar = ({ setIsOpen }) => {
       initial={{ width: "0%", x: 0 }}
       animate={{ width: "100%", x: 0 }}
       transition={{ duration: 3, ease: "easeInOut" }}
-      className="flex flex-col   items-center justify-center shadow-right text-gray-500 border-r-1 border-color-wheat"
+      className="flex flex-col min-h-screen  items-center justify-center shadow-right text-gray-500 border-r-1 border-color-wheat"
     >
       {/* Profile Image and Name */}
-      <div className="w-full   text-center relative">
+      <div className="w-full    text-center relative">
         <img src={ProfileImage} alt="" className="h-60 w-60 object-cover" />
         <div className="text-lg text-white bg-blue-400 w-full h-10 absolute bottom-0 flex items-center justify-center opacity-60">
           <h1 className="absolute bottom-3 text-white">Shantanu c </h1>
@@ -89,7 +89,7 @@ const SideBar = ({ setIsOpen }) => {
         ))}
 
         {/* Social Media Icons */}
-        <div className="flex  gap-3 items-center px-8 py-4">
+        <div className="flex absolute bottom-0 gap-3 items-center px-8 py-4">
           {socialMediaIcons.map((item, index) => (
             <a
               key={index}
