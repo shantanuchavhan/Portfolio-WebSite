6b27e042772052eef656d96b5f7792f92435f5ae
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-26 19:44:52 +0530

    commit

--- src/components/HomeLayout/HomeLayout.jsx
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -38,6 +38,7 @@ const HomeLayout = () => {
       </div>
       <div className="w-full z-1" >
   <div className="text-white" style={{ overflow: 'scroll' }}>
+  {isOpen && (<div className="fixed inset-0 bg-black z-[-1] opacity-75 h-full"></div>)}
     <Outlet />
   </div>
 </div>
