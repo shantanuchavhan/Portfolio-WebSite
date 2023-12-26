698a4bcac61e7f17b434be0c261d827aab189ac4
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-26 22:07:57 +0530

    commit

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -54,7 +54,7 @@ const SideBar = ({ setIsOpen }) => {
       initial={{ width: "0%", x: 0 }}
       animate={{ width: "100%", x: 0 }}
       transition={{ duration: 3, ease: "easeInOut" }}
-      className="flex flex-col min-h-screen  relative  items-center justify-center shadow-right text-gray-500 border-r-1 border-color-wheat"
+      className="flex flex-col min-h-screen fixed top-0  bottom-0    relative  items-center justify-center shadow-right text-gray-500 border-r-1 border-color-wheat"
     >
       {/* Profile Image and Name */}
       <div className="w-full absolute  text-center relative">
