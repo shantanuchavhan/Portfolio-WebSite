8a69d49f28838efec641d000d08a7ed4ea4ee373
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-26 13:36:39 +0530

    commit

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Icon from "../../images/suitcase.png";
 import AnimatedSection from "../../components/AnimatedSection/AnimatedSection";
+import { motion } from "framer-motion";
 
 const Portfolio = () => {
   const [active, setActive] = useState("ALL");
@@ -10,16 +11,21 @@ const Portfolio = () => {
     <AnimatedSection Icon={Icon} sectionName={"Portfolio"}>
       <div className="flex flex-col gap-6">
         <div className="flex items-center justify-center pt-10">
-          <div className="flex rounded-xl gap-2 border border-gray-300 border-dotted">
+          <div className="flex rounded-xl gap-2 border border-gray-300 border-dotted overflow-hidden">
             {/* Use map to generate buttons dynamically */}
             {sections.map((section,index) => (
-              <h2
+              <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 2, ease: 'easeInOut' }}
+                
+            
                 key={index}
-                className={`${active === section ? "bg-blue-400" : ""}  p-3 rounded-lg`}
+                className={`${active === section ? "bg-sky-400" : ""}  p-3 rounded-lg hover`}
                 onClick={() => setActive(section)}
               >
                 {section}
-              </h2>
+              </motion.div>
             ))}
           </div>
         </div>
