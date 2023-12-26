9487f5876ec47a655bb5028d0bd2622d347cc561
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-26 19:13:34 +0530

    commit

--- src/components/HomeLayout/HomeLayout.jsx
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -36,11 +36,12 @@ const HomeLayout = () => {
       <div className={`${isOpen ? "block fixed top-0" : "hidden  w-1/6" }  lg:block w-60 z-10 overflow-hidden   shadow-lg`}>
         <SideBar />
       </div>
-      <div className="w-full z-1  overflow-scroll">
-        <div className="text-white">
-          <Outlet />
-        </div>
-      </div>
+      <div className="w-full z-1" style={{ overflow: 'scroll' }}>
+  <div className="text-white">
+    <Outlet />
+  </div>
+</div>
+
     </div>
   );
 };
