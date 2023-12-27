14fd3840c2bf00bce15ecedbd6c83eba6d3799de
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-27 09:49:44 +0530

    commit

--- src/components/AnimatedSection/AnimatedSection.jsx
+++ src/components/AnimatedSection/AnimatedSection.jsx
@@ -21,7 +21,7 @@ const AnimatedSection = ({ Icon, sectionName, children }) => {
         <Header Icon={Icon} sectonName={sectionName} />
         {children}
       </motion.div>
-      <div className="fixed inset-0 bg-black z-[-1] opacity-75 h-full w-screen"></div>
+      
     </div>
   );
 };
