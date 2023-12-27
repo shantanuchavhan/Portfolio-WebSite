2590de4cc998725003710feb566892a2462135b1
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-27 13:56:06 +0530

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ Icon, sectonName }) => {
   return (
     <div className="flex justify-between items-end  h-1/6 border-b border-dotted border-[#f8fee9] pt-10 py-2">
-      <h1 className="text-4xl my-2 ">{sectonName}</h1>
+      <h1 className="text-3xl lg:text-4xl my-2 ">{sectonName}</h1>
       <div>
         <img className=" h-8 w-8 my-2  " src={Icon} alt="" />
       </div>
