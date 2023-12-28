4aeff55a9fc58237f9b9b85aef0ef3651699f32c
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-28 12:19:20 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import "./App.css";
-
-
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import Loading from "./components/Loading/Loading";
 import Home from "./pages/Home/Home";
 import AbouteMe from "./pages/AbouteMe/AbouteMe";
 import Resume from "./pages/Resume/Resume";
@@ -12,29 +10,43 @@ import Contact from "./pages/Contact/Contact";
 import bg from "./images/pexels-pixabay-326333.jpg";
 import HomeLayout from "./components/HomeLayout/HomeLayout";
 import { SideBarProvider } from "./context/SideBarProvider";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Set a timeout to hide the loading screen after 3 seconds
+    const loadingTimeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    // Clean up the timeout to avoid memory leaks
+    return () => clearTimeout(loadingTimeout);
+  }, []);
+
   return (
-    <div className="App w-screen relative   h-screen">
-      <SideBarProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomeLayout />}>
-            <Route index element={<Home />} />
-            <Route path="about-me" element={<AbouteMe />} />
-            <Route path="resume" element={<Resume />} />
-            <Route path="portfolio" element={<Portfolio />} />
-            <Route path="blogs" element={<Blog />} />
-            <Route path="contact" element={<Contact />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      </SideBarProvider>
-      <div className="fixed top-0 bottom-0 opacity-75  left-0 z-[-10]">
-        <img className="object-cover left-0 z-[-10] h-screen w-screen" src={bg} alt="" />
-        
-      </div>
-      
-      
+    <div className={`App w-screen relative h-screen ${isLoading ? 'overflow-hidden' : ''}`}>
+      {isLoading ? (
+        <Loading /> // Replace with your loading component or message
+      ) : (
+        <SideBarProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomeLayout />}>
+                <Route index element={<Home />} />
+                <Route path="about-me" element={<AbouteMe />} />
+                <Route path="resume" element={<Resume />} />
+                <Route path="portfolio" element={<Portfolio />} />
+                <Route path="blogs" element={<Blog />} />
+                <Route path="contact" element={<Contact />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+          <div className="fixed top-0 bottom-0 opacity-75 left-0 z-[-10]">
+            <img className="object-cover left-0 z-[-10] h-screen w-screen" src={bg} alt="" />
+          </div>
+        </SideBarProvider>
+      )}
     </div>
   );
 }
---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactLoading from "react-loading";
+
+const Loading = () => {
+  return (
+    <div>
+    <h2>Loading in ReactJs - GeeksforGeeks</h2>
+    <ReactLoading type="balls" color="#0000FF"
+        height={100} width={50} />
+    <ReactLoading type="bars" color="#0000FF"
+        height={100} width={50} />
+    <ReactLoading type="bubbles" color="#0000FF"
+        height={100} width={50} />
+    <ReactLoading type="cubes" color="#0000FF"
+        height={100} width={50} />
+    <ReactLoading type="cylon" color="#0000FF"
+        height={100} width={50} />
+    <ReactLoading type="spin" color="#0000FF"
+        height={100} width={50} />
+    <ReactLoading type="spokes" color="#0000FF"
+        height={100} width={50} />
+    <ReactLoading
+        type="spinningBubbles"
+        color="#0000FF"
+        height={100}
+        width={50}
+    />
+</div>
+  )
+}
+
+export default Loading
