1b9c308d58af6bc1104e55419de51409d6a04cf6
shantanuchavhan <shantanuchavhan10@gmail.com>
2023-12-30 13:37:48 +0530

    commit

--- src/components/HomeLayout/HomeLayout.jsx
+++ src/components/HomeLayout/HomeLayout.jsx
@@ -68,10 +68,10 @@ const HomeLayout = () => {
               <div className={`bg-white text-black h-32 p-2  flex flex-col gap-1 z-30`}>
                 <h2>choose color</h2>
                 <div className="flex gap-3">
-                  <div className="bg-green-400 h-4 w-4" onClick={()=>setColor("bg-green-400")}></div>
-                  <div className="bg-yellow-400 h-4 w-4" onClick={()=>setColor("bg-yellow-400")}></div>
-                  <div className="bg-red-400 h-4 w-4" onClick={()=>setColor("bg-red-400")}></div>
-                  <div className="bg-blue-400 h-4 w-4" onClick={()=>setColor("bg-blue-400")}></div>
+                  <div className="bg-green-300 h-4 w-4" onClick={()=>setColor("bg-green-300")}></div>
+                  <div className="bg-yellow-300 h-4 w-4" onClick={()=>setColor("bg-yellow-300")}></div>
+                  <div className="bg-red-300 h-4 w-4" onClick={()=>setColor("bg-red-300")}></div>
+                  <div className="bg-blue-300 h-4 w-4" onClick={()=>setColor("bg-blue-300")}></div>
                 </div>
                 <div>
                   <h4>choose language</h4>
