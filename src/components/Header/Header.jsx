363d1d913770ac41b62961b558487b1b0a1c07c5
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-03 15:32:27 +0530

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { useTranslation } from 'react-i18next';
 
 
 const Header = ({ Icon, sectonName }) => {
-  
+  const { t } = useTranslation();
   return (
     <div className="flex justify-between items-end  h-1/6 border-b border-dotted border-[#f8fee9] pt-10 lg:pt-6 py-2">
       <h1 className="text-[28px] lg:text-[34px] my-2 font-dancing-script ">
-        {sectonName}
+        {t({sectonName})}
       </h1>
       <div>
         <img
