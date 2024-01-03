abe063fda10c63cede452a1390c85c4d2280f03b
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-03 19:02:31 +0530

    commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, {useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import Typewriter from "typewriter-effect";
 import { useSideBarContext } from "../../context/SideBarProvider";
