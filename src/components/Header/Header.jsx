387800057bcda9b380b4436b4d3a81a2dae837f0
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-03 18:12:51 +0530

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,13 @@ const Header = ({ Icon, sectonName }) => {
   const { t} = useTranslation();
   console.log(capitalizeFirstLetter(t(sectonName)),"sectonName")
   const capitalizeFirstLetter = (string) => {
+    string=t(string)
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
   return (
     <div className="flex justify-between items-end  h-1/6 border-b border-dotted border-[#f8fee9] pt-10 lg:pt-6 py-2">
       <h1 className="text-[28px] lg:text-[34px] my-2 font-dancing-script ">
-      {capitalizeFirstLetter(t(sectonName))}
+      {capitalizeFirstLetter(sectonName)}
       </h1>
       <div>
         <img
