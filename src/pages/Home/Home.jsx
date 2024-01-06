e1e3552e69671b82baf48427fbcf430194fd0aba
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-06 17:23:46 +0530

    commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -72,7 +72,7 @@ const Home = () => {
           initial={{ opacity: 0, y: 100, color: "white" }}
           animate={{ opacity: 1, y: 0, color: "white" }}
           transition={{duration:2,ease: "easeInOut", delay: 6}}
-          className="border border-blue-300 p-2 w-full"
+          className="border border-blue-300 p-2"
 
           >
             Hire me.</motion.h1>
@@ -81,7 +81,7 @@ const Home = () => {
           initial={{ opacity: 0, y: 100, color: "white" }}
           animate={{ opacity: 1, y: 0, color: "white" }}
           transition={{duration:2,ease: "easeInOut", delay: 7}}
-          onClick={()=>setIsOpen(true)} className="lg:hidden border border-blue-300 p-2 w-full"
+          onClick={()=>setIsOpen(true)} className="lg:hidden border border-blue-300 p-2 "
           
           >Explore.</motion.h1>
 
