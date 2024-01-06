7380bf6cb45f6f9c34fbe13960d84c1bdd01f76c
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-06 18:08:38 +0530

    commit

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -8,21 +8,26 @@ const Resume = () => {
     <div>
       <div>
         <h1>Education :- </h1>
-      <div>
-        <h1>Bachler of Electronic Engineering</h1>
-        <h2>From:2019 to 2022</h2>
-        <h2>Score:7.6 CGPA</h2>
+          <div>
+            <h1>Bachler of Electronic Engineering</h1>
+            <h2>From:2019 to 2022</h2>
+            <h2>Score:7.6 CGPA</h2>
+          </div>
+          <div>
+            <h1>Diploma in Electronic Engineering</h1>
+            <h2>From:2016 to 2019</h2>
+            <h2>Score:71.71 % </h2>
+          </div>
+          <div>
+            <h1>SSC</h1>
+            <h2>From:2015 to 2016</h2>
+            <h2>Score:82.80 % </h2>
+          </div>
       </div>
       <div>
-        <h1>Diploma in Electronic Engineering</h1>
-        <h2>From:2016 to 2019</h2>
-        <h2>Score:71.71 % </h2>
-      </div>
-      <div>
-        <h1>SSC</h1>
-        <h2>From:2015 to 2016</h2>
-        <h2>Score:82.80 % </h2>
-      </div>
+        <h1>Experiance</h1>
+        
+
       </div>
     </div>
   </AnimatedSection>;
