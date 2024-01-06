70147f2299eaedc209045b6187561db3648d740b
shantanuchavhan <shantanuchavhan10@gmail.com>
2024-01-06 18:21:40 +0530

    commit

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -100,7 +100,7 @@ const Contact = () => {
             </div>
             <button
               type="submit"
-              className={`py-3 ${color} px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`}
+              className={`py-3 bg-${color} px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`}
             >
               {t("Send message")}
             </button>
